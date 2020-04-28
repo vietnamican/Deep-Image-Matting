879ea549ac8244fb14d87d40b8a562cc874083d7
vietnamican <vietnamican@gmail.com>
2020-04-28 16:25:05 +0700

    adding tpu in google colab

---
+++ train_segnet_v5_tpu.py
@@ -0,0 +1,77 @@
+import argparse
+import os
+
+import tensorflow.keras as keras
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.utils import multi_gpu_model
+
+from config import patience, batch_size, epochs, num_train_samples, num_valid_samples
+from data_generator import train_gen, valid_gen
+from migrate import migrate_model
+from segnet_v5 import build_encoder_decoder, build_refinement
+from utils import overall_loss, get_available_cpus, get_available_gpus
+
+try:
+  tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+  print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
+except ValueError:
+  raise BaseException('ERROR: Not connected to a TPU runtime; please see the previous cell in this notebook for instructions!')
+
+tf.config.experimental_connect_to_cluster(tpu)
+tf.tpu.experimental.initialize_tpu_system(tpu)
+tpu_strategy = tf.distribute.experimental.TPUStrategy(tpu)
+
+log_dir = '/content/drive/Shared drives/DNN/Deep-Image-Matting/logs_5'
+checkpoint_models_path = '/content/drive/Shared drives/DNN/Deep-Image-Matting/checkpoints_5/'
+
+if __name__ == '__main__':
+    # Parse arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--pretrained", help="path to save pretrained model files")
+    args = vars(ap.parse_args())
+    pretrained_path = args["pretrained"]
+
+    # Callbacks
+    tensor_board = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
+    model_names = checkpoint_models_path + 'final.{epoch:02d}-{val_loss:.4f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
+
+
+    class MyCbk(keras.callbacks.Callback):
+        def __init__(self, model):
+            keras.callbacks.Callback.__init__(self)
+            self.model_to_save = model
+
+        def on_epoch_end(self, epoch, logs=None):
+            fmt = checkpoint_models_path + 'final.%02d-%.4f.hdf5'
+            self.model_to_save.save(fmt % (epoch, logs['val_loss']))
+
+
+    # Load our model, added support for Multi-GPUs
+    num_gpu = len(get_available_gpus())
+    with tpu_strategy.scope():
+        model = build_encoder_decoder()
+        final = build_refinement(model)
+        if pretrained_path is not None:
+            final.load_weights(pretrained_path)
+    final.compile(optimizer='nadam', loss=overall_loss)
+
+    print(final.summary())
+
+    # Final callbacks
+    callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr]
+
+    # Start Fine-tuning
+    final.fit(train_gen(),
+                        steps_per_epoch=num_train_samples // batch_size,
+                        validation_data=valid_gen(),
+                        validation_steps=num_valid_samples // batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        callbacks=callbacks,
+                        use_multiprocessing=True,
+                        workers=2
+                        )
