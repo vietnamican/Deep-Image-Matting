a916d32d81a4fdfbb6cc2c89333f2de00e48a93b
vietnamican <vietnamican@gmail.com>
2020-05-01 22:03:42 +0700

    nothing

--- train.py
+++ train.py
@@ -3,17 +3,18 @@ import os
 
 import tensorflow.keras as keras
 import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, TensorBoard
 from tensorflow.keras.utils import multi_gpu_model
 
 from config import patience, batch_size, epochs, num_train_samples, num_valid_samples
-from data_generator import train_gen, valid_gen
+from data_generator_2 import train_gen, valid_gen
 from migrate import migrate_model
 from segnet import build_encoder_decoder, build_refinement
-from utils import overall_loss, get_available_cpus, get_available_gpus
+from utils import overall_loss, get_available_cpus, get_available_gpus, get_initial_epoch
 
 log_dir = './logs_1'
-checkpoint_models_path = './checkpoints_1/'
+checkpoint_models_path = './checkpoints_1/cp-{epoch:04d}-{val_loss:.4f}.ckpt'
+checkpoint_dir = os.path.dirname(checkpoint_models_path)
 
 if __name__ == '__main__':
     # Parse arguments
@@ -23,9 +24,9 @@ if __name__ == '__main__':
     pretrained_path = args["pretrained"]
 
     # Callbacks
-    tensor_board = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
-    model_names = checkpoint_models_path + 'final.{epoch:02d}-{val_loss:.4f}.hdf5'
-    model_checkpoint = ModelCheckpoint(model_names, monitor='val_loss', verbose=1, save_best_only=True)
+    tensor_board = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
+    # model_names = checkpoint_models_path + 'final.{epoch:02d}-{val_loss:.4f}.hdf5'
+    model_checkpoint = ModelCheckpoint(filepath=checkpoint_models_path, monitor='val_loss', verbose=1, save_weights_only=True)
     early_stop = EarlyStopping('val_loss', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
 
@@ -46,8 +47,8 @@ if __name__ == '__main__':
         with tf.device("/cpu:0"):
             model = build_encoder_decoder()
             model = build_refinement(model)
-            if pretrained_path is not None:
-                model.load_weights(pretrained_path)
+            # if pretrained_path is not None:
+            #     model.load_weights(pretrained_path)
 
         final = multi_gpu_model(model, gpus=num_gpu)
         # rewrite the callback: saving through the original model and not the multi-gpu model.
@@ -55,8 +56,14 @@ if __name__ == '__main__':
     else:
         model = build_encoder_decoder()
         final = build_refinement(model)
-        if pretrained_path is not None:
-            final.load_weights(pretrained_path)
+        # if pretrained_path is not None:
+        #     final.load_weights(pretrained_path)
+    if len(os.listdir(checkpoint_dir)) > 0:
+        latest = tf.train.latest_checkpoint(checkpoint_dir)
+        final.load_weights(latest)
+        initial_epoch = get_initial_epoch(latest)
+    else:
+        initial_epoch = 0
     final.compile(optimizer='nadam', loss=overall_loss)
 
     print(final.summary())
@@ -65,13 +72,14 @@ if __name__ == '__main__':
     callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr]
 
     # Start Fine-tuning
-    final.fit_generator(train_gen(),
+    final.fit(train_gen(),
                         steps_per_epoch=num_train_samples // batch_size,
                         validation_data=valid_gen(),
                         validation_steps=num_valid_samples // batch_size,
                         epochs=epochs,
                         verbose=1,
                         callbacks=callbacks,
+                        initial_epoch=initial_epoch,
                         use_multiprocessing=True,
                         workers=2
                         )
