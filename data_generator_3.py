328144a9e208020547b05971932f4110a2e5de30
vietnamican <vietnamican@gmail.com>
2020-05-06 09:38:31 +0700

    nothing

--- data_generator_3.py
+++ data_generator_3.py
@@ -145,7 +145,7 @@ class DataGenSequence(Sequence):
         filename = '{}_names.txt'.format(usage)
         with open(filename, 'r') as f:
             self.names = f.read().splitlines()
-            self.names = self.names[:38790]
+            self.names = self.names[:34480]
 
         np.random.shuffle(self.names)
 
